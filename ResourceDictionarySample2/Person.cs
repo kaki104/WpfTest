2900c93be3f888ea14efb7134e041e5eb3b2a2f1
Connor Park <kaki104@hanmail.net>
2022-02-11 00:22:29 +0900

    리소스 딕셔너리 활용 추가

---
+++ ResourceDictionarySample2/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceDictionarySample2
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Sex { get; set; }
+        public string Phone { get; set; }
+    }
+}
