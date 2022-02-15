059ae133bc18ad6975d2784406844a7c26d1a08c
Connor Park <kaki104@hanmail.net>
2022-02-15 23:10:11 +0900

    ListView, ListBox, DataGrid 차이점 확인용 셈플 프로젝트 추가

---
+++ ListViewSample/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewSample
+{
+    public class Animal
+    {
+        public bool IsChecked { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
---
+++ ListViewSample/ListView1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ListViewSample
+{
+    public partial class ListView1 : Window
+    {
+        private List<Animal> _list;
+
+        public ListView1()
+        {
+            InitializeComponent();
+
+            _list = new List<Animal>
+            {
+                new Animal { IsChecked=true, Name = "Cat",  Type = "animal", ImagePath = @"Images\cat.png"},
+                new Animal { IsChecked=false, Name = "Dog",  Type = "animal", ImagePath = @"Images\dog.png"},
+                new Animal { IsChecked=true, Name = "Fish",  Type = "fish", ImagePath = @"Images\fish.png"},
+                new Animal { IsChecked=false, Name = "Flower",  Type = "plant", ImagePath = @"Images\flower.jpg"},
+            };
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListView01.ItemsSource = _list;
+            ListView02.ItemsSource = _list;
+            ListBox.ItemsSource = _list;
+            DataGrid.ItemsSource = _list;
+        }
+    }
+}
