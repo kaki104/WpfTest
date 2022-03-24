5d7d738868945e3f54123c084476877347bc41bb
Connor Park <kaki104@hanmail.net>
2022-03-24 11:44:34 +0900

    .NET 5 Application 버전(빌드 일시) 생성하고 사용하기

---
+++ CreateBuildDate/Builtin.cs
@@ -0,0 +1,11 @@
+﻿namespace CreateBuildDate
+{
+    ///// <summary>
+    ///// 프로그램에서 사용하기 위해 partial class를 미리 만들어 놓음 
+    ///// Visual Studio 2019에서 Builtin을 찾지 못해 에러가 발생하면 추가해서 사용
+    ///// </summary>
+    //public static partial class Builtin
+    //{
+    //    public static long CompileTime { get; }
+    //}
+}
---
+++ CreateBuildDate/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace CreateBuildDate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var buildDate = $"ver. {new DateTime(Builtin.CompileTime, DateTimeKind.Utc).ToString("yyyy-MM-dd-HHmm")}";
+            BuildDate.Text = buildDate;
+        }
+    }
+}
