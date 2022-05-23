f14f5cc43945c938a886de765cd40f54587ce256
Connor Park <kaki104@hanmail.net>
2022-05-23 17:26:30 +0900

    이벤트 드리븐 셈플 프로젝트 추가

---
+++ CrudSample.Core/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudSample.Core.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime RegDate { get; set; }
+        public bool IsUse { get; set; }
+    }
+}
