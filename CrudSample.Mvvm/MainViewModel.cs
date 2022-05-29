f8afe3ecd9bb163b35acf459a95df8855951f057
Connor Park <kaki104@hanmail.net>
2022-05-29 23:07:29 +0900

    이벤트 드리븐과 MVVM 패턴 사용 비교 분석 예제

---
+++ CrudSample.Mvvm/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CrudSample.Core.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudSample.Mvvm
+{
+    public class MainViewModel : ObservableObject
+    {
+        //public IList<Member> Members { get; set; }
+        private IList<Member> _member;
+        public IList<Member> Members
+        {
+            get { return _member; }
+            set { SetProperty(ref _member, value); }
+        }
+
+        public IRelayCommand NewCommand { get; set; }
+
+        public IRelayCommand CancelCommand { get; set; }
+
+        private bool _isEditing;
+        public bool IsEditing
+        {
+            get { return _isEditing; }
+            set { SetProperty(ref _isEditing, value); }
+        }
+
+        public MainViewModel()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            Members = new ObservableCollection<Member> 
+            {
+                new Member
+                {
+                    Id = 1,
+                    Name = "kaki104",
+                    Phone = "010-1111-2222",
+                    RegDate = DateTime.Now,
+                    IsUse = true,
+                }
+            };
+
+            NewCommand = new RelayCommand(() => IsEditing = true);
+            CancelCommand = new RelayCommand(() => IsEditing = false);
+        }
+    }
+}
---
+++ CrudSample.Mvvm/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace CrudSample.Mvvm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = App.Current.Services.GetService(typeof(MainViewModel));
+        }
+    }
+}
