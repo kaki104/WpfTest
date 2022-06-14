98a2379b9cea84a5f66dbc98c8ef412e15d508cd
Connor Park <kaki104@hanmail.net>
2022-06-15 00:18:45 +0900

    MVVM Pattern을 사용하는 개발자를 위한 안내 V1.0 part4-2 Data Binding

---
+++ ElementNameSample/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementNameSample
+{
+    public class MainViewModel : ObservableObject
+    {
+        public IList<Person> People { get; set; }
+
+        public MainViewModel()
+        {
+            People = new List<Person>
+            {
+                new Person{ Id = 1, Name = "kaki104", Sex = true },
+                new Person{ Id = 2, Name = "kaki105", Sex = false },
+                new Person{ Id = 3, Name = "kaki106", Sex = true },
+                new Person{ Id = 4, Name = "kaki107", Sex = false },
+                new Person{ Id = 5, Name = "kaki108", Sex = true },
+            };
+        }
+    }
+}
---
+++ SourceSample/MessageExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SourceSample
+{
+    public class MessageExt
+    {
+        public static string GetMessage(DependencyObject obj)
+        {
+            return (string)obj.GetValue(MessageProperty);
+        }
+
+        public static void SetMessage(DependencyObject obj, string value)
+        {
+            obj.SetValue(MessageProperty, value);
+        }
+        
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.RegisterAttached("Message", typeof(string), typeof(MessageExt), new PropertyMetadata(null));
+
+    }
+}
