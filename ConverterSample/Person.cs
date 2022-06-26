9f7e7d61e8a894dd2b95fc79ea5b8feea428aec7
Connor Park <kaki104@hanmail.net>
2022-06-26 12:01:14 +0900

    블로그 포스팅용 셈플 추가 - 컨버터

---
+++ ConverterSample/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterSample
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool Sex { get; set; } = false;
+        public string DepartmentCode { get; set; } = string.Empty;
+    }
+}
