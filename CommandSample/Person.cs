eca604053179be4a05b478be7ca0bfef7441a4c3
Connor Park <kaki104@hanmail.net>
2022-07-03 22:58:32 +0900

    Command 사용 예제 프로젝트 추가

---
+++ CommandSample/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandSample
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Sex { get; set; }
+        public string Description { get; set; }
+    }
+}
