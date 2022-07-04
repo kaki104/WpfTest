7d48330be76678ecbf8471ffe0e851560fb6e2b6
Connor Park <kaki104@hanmail.net>
2022-07-05 06:50:52 +0900

    DeleteCommand 추가

--- CommandSample/MainViewModel.cs
+++ CommandSample/MainViewModel.cs
@@ -1,11 +1,10 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -22,6 +21,25 @@ namespace CommandSample
             get { return _people; }
             set { SetProperty(ref _people, value); }
         }
+
+        private Person _selectedPerson;
+        /// <summary>
+        /// 선택된 Person
+        /// </summary>
+        public Person SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                SetProperty(_selectedPerson, value,
+                    person =>
+                    {
+                        _selectedPerson = person;
+                        DeleteCommand.NotifyCanExecuteChanged();
+                    });
+            }
+        }
+
         public IRelayCommand NormalCommand { get; set; }
 
         public IAsyncRelayCommand AsyncCommand { get; set; }
@@ -29,6 +47,8 @@ namespace CommandSample
         public ICommand SelectionChangedCommand { get; set; }
 
         public ICommand AddingNewItemCommand { get; set; }
+
+        public IRelayCommand DeleteCommand { get; set; }
 
         private string _message;
         public string Message
@@ -47,11 +67,13 @@ namespace CommandSample
         public MainViewModel()
         {
             NormalCommand = new RelayCommand(OnNormal);
+            NormalCommand = new RelayCommand(async () => await OnNormalAsync());
             AsyncCommand = new AsyncRelayCommand(OnNormalAsync);
             SelectionChangedCommand = new RelayCommand<Person>(OnSelectionChanged);
             AddingNewItemCommand = new RelayCommand<object>(OnAddingNewItem);
+            DeleteCommand = new RelayCommand(OnDelete, () => SelectedPerson != null);
 
-            People = new List<Person>
+            People = new ObservableCollection<Person>
             {
                 new Person { Id = 1, Name = "kaki104", Sex = true, Description = "hehehe" },
                 new Person { Id = 2, Name = "kaki105", Sex = false, Description = "hahaha" },
@@ -61,9 +83,18 @@ namespace CommandSample
             };
         }
 
+        private void OnDelete()
+        {
+            if (MessageBox.Show("선택한 아이템을 삭제하시겠습니까?", "삭제 확인", MessageBoxButton.YesNo, MessageBoxImage.Question)
+                == MessageBoxResult.Yes)
+            {
+                People.Remove(SelectedPerson);
+            }
+        }
+
         private void OnAddingNewItem(object obj)
         {
-            if(obj is AddingNewItemEventArgs args)
+            if (obj is AddingNewItemEventArgs args)
             {
                 args.NewItem = new Person { Id = People.Max(p => p.Id) + 1 };
             }
@@ -72,6 +103,7 @@ namespace CommandSample
         private void OnSelectionChanged(Person obj)
         {
             Message = obj == null ? "선택한 아이템이 없습니다." : $"{obj.Name}을 선택했습니다.";
+            SelectedPerson = obj;
         }
 
         private async Task OnNormalAsync()
@@ -85,5 +117,6 @@ namespace CommandSample
         {
             Message = "Normal Button을 클릭했습니다.";
         }
+
     }
 }
