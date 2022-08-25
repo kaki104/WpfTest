047f600eb79c4943db7ddadeac150e1e6721432a
Connor Park <kaki104@modetour.com>
2022-08-25 09:26:03 +0900

    셀렉터 셈플 추가

---
+++ SelectorSample/MessageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectorSample
+{
+    public class MessageModel
+    {
+        public int Id { get; set; }
+        public string MessageType { get; set; }
+        public string Message { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+    }
+}
