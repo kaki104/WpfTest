89f852deda7353cf8d5f1985a4d8994323184cb5
Connor Park <kaki104@modetour.com>
2022-10-21 08:53:34 +0900

    텔레릭 gridview 셈플 작업 중

---
+++ TelerikSample1/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TelerikSample1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+            //this.InitializeComponent();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var mainWindow = Services.GetService<MainWindow>();
+            if (mainWindow != null)
+            {
+                mainWindow.Show();
+            }
+            else
+            {
+                Shutdown();
+            }
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton(typeof(MainWindow));
+            services.AddSingleton(typeof(MainWindowViewModel));
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ TelerikSample1/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection.Metadata;
+using System.Windows.Input;
+
+namespace TelerikSample1
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private IList<Person> _people;
+        /// <summary>
+        /// 피플~
+        /// </summary>
+        public IList<Person> People
+        {
+            get { return _people; }
+            set { SetProperty(ref _people, value); }
+        }
+
+        private string _message;
+        /// <summary>
+        /// 결과 출력용 메시지
+        /// </summary>
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+        /// <summary>
+        /// 서비스 프로바이더
+        /// </summary>
+        private readonly IServiceProvider _serviceProvider;
+
+        public MainWindowViewModel()
+        {
+
+        }
+        public MainWindowViewModel(IServiceProvider serviceProvider) : this()
+        {
+            //App.Current.Services.GetService를 직접 사용하지 않고, Injection 받아서 사용합니다.
+            _serviceProvider = serviceProvider;
+            Init();
+        }
+
+        private void Init()
+        {
+            People = new ObservableCollection<Person>
+            {
+                new Person { Id = 1, Name = "kaki104", Sex = true, Description = "hehehe" },
+                new Person { Id = 2, Name = "kaki105", Sex = false, Description = "hahaha" },
+                new Person { Id = 3, Name = "kaki106", Sex = true, Description = "hohoho" },
+                new Person { Id = 4, Name = "kaki107", Sex = false, Description = "kekeke" },
+                new Person { Id = 5, Name = "kaki108", Sex = true, Description = "kokoko" },
+            };
+        }
+    }
+}
