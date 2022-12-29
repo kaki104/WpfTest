1000a4e36a90732e81d2021a85cc3b3d5854aff9
Connor Park <kaki104@modetour.com>
2022-12-29 09:44:22 +0900

    DataTrigger 셈플 추가

---
+++ TriggerSample3/ListToBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TriggerSample3
+{
+    /// <summary>
+    /// List 데이터 타입 존재 여부와 카운트 여부를 bool로 변환, null, 0이면 false, 0 크면 true
+    /// </summary>
+    public class ListToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is IList list && list.Count > 0 ? true : (object)false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TriggerSample3/Person.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TriggerSample3
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public bool? Sex { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+         
+        public int Age { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public IList<object> Childs { get; set; }
+    }
+}
