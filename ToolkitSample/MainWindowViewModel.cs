51b97cb5fffb9a63d1ff2277d158a0a42978fa3b
Connor Park <kaki104@hanmail.net>
2023-01-14 16:32:57 +0900

    Toolkit sample 프로젝트 추가

---
+++ ToolkitSample/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+
+namespace ToolkitSample
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private IList<Person> _people;
+        /// <summary>
+        /// People
+        /// </summary>
+        public IList<Person> People
+        {
+            get { return _people; }
+            set { SetProperty(ref _people, value); }
+        }
+
+        private Person _selectedPerson;
+        /// <summary>
+        /// 선택된 사람
+        /// </summary>
+        public Person SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set { SetProperty(ref _selectedPerson, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            People = new List<Person>
+            {
+                new Person{ Id = 1, Name = "kaki104", Sex = true},
+                new Person{ Id = 2, Name = "kaki105", Sex = false},
+                new Person{ Id = 3, Name = "kaki106", Sex = true},
+                new Person{ Id = 4, Name = "kaki107", Sex = false},
+                new Person{ Id = 5, Name = "kaki108", Sex = true},
+            };
+        }
+    }
+}
---
+++ ToolkitSample/Person.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ToolkitSample
+{
+    public class Person : ObservableObject
+    {
+        private int _id;
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id
+        {
+            get { return _id; }
+            set { SetProperty(ref _id, value); }
+        }
+
+        private string _name;
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        private bool _sex;
+        /// <summary>
+        /// Sex
+        /// </summary>
+        public bool Sex
+        {
+            get { return _sex; }
+            set { SetProperty(ref _sex, value); }
+        }
+    }
+}
