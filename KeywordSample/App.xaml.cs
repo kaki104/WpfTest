9ba56abd2a54947128c07b47dafb8a08f69c812c
Connor Park <kaki104@modetour.com>
2023-02-21 09:02:57 +0900

    TextBlock에 표시되는 내용 중 검색어를 빨간색으로 표시하기

---
+++ KeywordSample/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using KeywordSample.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace KeywordSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ KeywordSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Mvvm;
+
+namespace KeywordSample.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _keyword;
+        /// <summary>
+        /// 키워드
+        /// </summary>
+        public string Keyword
+        {
+            get { return _keyword; }
+            set { SetProperty(ref _keyword, value); }
+        }
+
+        private string _sampleData;
+        public string SampleData
+        {
+            get { return _sampleData; }
+            set { SetProperty(ref _sampleData, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
