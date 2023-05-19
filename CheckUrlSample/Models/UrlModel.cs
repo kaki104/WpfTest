34753a9b7d564eaac16c1ef07a5e99679f1100d7
Connor Park <kaki104@modetour.com>
2023-05-19 18:05:39 +0900

    CheckUrlSample 프로젝트 추가

---
+++ CheckUrlSample/Models/UrlModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Mvvm;
+
+namespace CheckUrlSample.Models
+{
+    /// <summary>
+    /// url 모델
+    /// </summary>
+    public class UrlModel : BindableBase
+    {
+        private string _url;
+        /// <summary>
+        /// url
+        /// </summary>
+        public string Url
+        {
+            get => _url;
+            set => SetProperty(ref _url, value);
+        }
+
+        private bool _exist;
+        /// <summary>
+        /// 존재여부
+        /// </summary>
+        public bool Exist
+        {
+            get => _exist;
+            set => SetProperty(ref _exist, value);
+        }
+    }
+}
---
+++ CheckUrlSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,100 @@
+﻿using Prism.Mvvm;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using Prism.Ioc;
+using System.Collections;
+using CheckUrlSample.Models;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Prism.Commands;
+using System.Linq;
+
+namespace CheckUrlSample.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private HttpClient _httpClient;
+        private IContainerProvider _containerProvider;
+        private string _title = "Check Url Sample";
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        private IList<UrlModel> _urls;
+        /// <summary>
+        /// Urls
+        /// </summary>
+        public IList<UrlModel> Urls
+        {
+            get { return _urls; }
+            set { SetProperty(ref _urls, value); }
+        }
+        /// <summary>
+        /// 확인 시작 커맨드
+        /// </summary>
+        public ICommand CheckCommand { get; set; }
+
+        public MainWindowViewModel(IContainerProvider containerProvider)
+        {
+            _containerProvider = containerProvider;
+            _httpClient = new HttpClient();
+
+            CheckCommand = new DelegateCommand(OnCheck);
+
+            Urls = new List<UrlModel>
+            {
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fchosunbiz%2F20230519060200835hnhs.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fdaejonilbo%2F20230519084619212deap.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fohmynews%2F20230519051504385stmu.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fmbn%2F20230519085338010wmzr.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fkookje%2F20230519085244687vtvo.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fjoongang%2F20230519050141577xbvp.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fmk%2F20230519085101509vynh.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fkukinews%2F20230519084703188cynf.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fjibs%2F20230519084833012igty.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fned%2F20230519084017377uwyu.jpg&amp;scode=media", Exist = false},
+                new UrlModel{ Url = "https://img1.daumcdn.net/thumb/S96x60ht.u/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fnews%2F202305%2F19%2Fned%2F20230519084017377uwyu.j&amp;scode=media", Exist = false},
+            };
+        }
+        /// <summary>
+        /// 존재여부 확인
+        /// </summary>
+        private async void OnCheck()
+        {
+            var tasks = Urls.Select(u => CheckUrlExistAsync(u.Url)).ToList();
+            var resuls = await Task.WhenAll(tasks);
+            for (int i = 0; i < resuls.Count(); i++)
+            {
+                Urls[i].Exist = resuls[i];
+            }
+        }
+
+        /// <summary>
+        /// Url 존재 여부 확인
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<bool> CheckUrlExistAsync(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException($"url is required");
+            }
+            try
+            {
+                //자동 dispose를 위해서 using 사용, head 값만 반환하는 요청 생성
+                using HttpRequestMessage request = new(HttpMethod.Head, url);
+                //httpclient는 1개만 만들어서 계속 사용하는 것이 권장됨
+                using HttpResponseMessage response = await _httpClient.SendAsync(request);
+                return response.EnsureSuccessStatusCode().IsSuccessStatusCode;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
